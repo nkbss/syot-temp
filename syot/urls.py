79960a180ea9d8749c3533e564473a4efd7a8bcb
fifiGith <fordfifi@hotmail.com>
2017-09-03 23:20:16 +0700

    fix logo.png location

--- syot/urls.py
+++ syot/urls.py
@@ -21,4 +21,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', include('home.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
